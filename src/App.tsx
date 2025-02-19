7f6f296f0a10432df8273845b4fca1a0f1821f3a
FSBM <shyamx444@gmail.com>
2025-02-19 20:28:54 +0530

    till api_fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
-// import SubmitPage from "./page/SubmitPage";
 import SearchPage from "./page/SearchPage";
 import Intro from "./page/IntroPage";
-// import SuccessPage from './page/SuccessPage'
 import ErrorPage from "./page/ErrorPage";
 import SearchResponse from "./page/SearchResultPage";
 import ResponsePage from "./page/ResponsePage";
 import './index.css';
+
+
 
 const pageVariants = {
   initial: { opacity: 1, y: 0 },  
@@ -15,7 +15,7 @@ const pageVariants = {
   exit: { opacity: 1, y: 0, transition: { duration: 0.2 } }  
 };
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 
 const PageWrapper = ({ children }: { children: ReactNode }) => (
   <motion.div
@@ -30,13 +30,70 @@ const PageWrapper = ({ children }: { children: ReactNode }) => (
 );
 
 const AnimatedRoutes = () => {
+  const Navigate = useNavigate();
   const location = useLocation();
+
+  useEffect(() => {
+    // Check if access_token is already in localStorage
+    const accessToken = localStorage.getItem("access_token");
+    document.cookie = `access_token=${accessToken}; path=/; domain=localhost; SameSite=Lax`
+
+    // const [quotes, setQuotes] = useState([]);
+
+    const fetchData = async () => {
+      const res = await fetch("https://magical-famous-emu.ngrok-free.app/quotes");
+      console.log(res);
+    }
+    fetchData();
+
+
+
+
+    if (accessToken) {
+      document.cookie = `access_token=${accessToken}; path=/; domain=localhost; SameSite=Lax`
+      Navigate("/submit");
+      console.log("Access token found in localStorage: " + accessToken);
+      chrome.cookies.getAll({}, (cookies) => {
+        cookies.forEach((cookie) => {
+          if (cookie.name === "refresh_token") {
+            localStorage.setItem("refresh_token", cookie.value);
+          }
+        });
+      })
+      
+      
+    }
+
+    const handleCookieChange = (changeInfo: any) => {
+      
+      if (changeInfo.cookie.name === "access_token") {
+        if (changeInfo.cookie.value) {
+          localStorage.setItem("access_token", changeInfo.cookie.value);
+          Navigate("/submit");
+          console.log("Got the updated access_token: " + changeInfo.cookie.value);
+        } else {
+          console.log("access_token has been removed");
+        }
+      }
+    };
+
+    if(!accessToken){
+      chrome.cookies.onChanged.addListener(handleCookieChange);
+    }
+
+    // Clean up the listener when component is unmounted
+    return () => {
+      if(!accessToken){
+        chrome.cookies.onChanged.removeListener(handleCookieChange);
+      }
+    };
+  },[]);
 
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<PageWrapper><ResponsePage /></PageWrapper>} />
-        <Route path="/intro" element={<PageWrapper><Intro /></PageWrapper>} />
+        <Route path="/" element={<PageWrapper><Intro /></PageWrapper>} />
+        <Route path="/submit" element={<PageWrapper><ResponsePage /></PageWrapper>} />
         <Route path="/search" element={<PageWrapper><SearchPage /></PageWrapper>} />
         <Route path="/success" element={<PageWrapper><ErrorPage /></PageWrapper>} />
         <Route path="/response" element={<PageWrapper><SearchResponse /></PageWrapper>} />
